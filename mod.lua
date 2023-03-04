6064e99f6604d288f6969ccbf94a84af71787f50
Florian Thiel <flth@flth.de>
2023-03-05 00:00:44 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,107 @@
+﻿function data()
+  return {  
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING", 
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = 'jay_',
+				role = 'CREATOR',
+				text = 'Modell',
+				tfnetId = 28954,
+			},
+					    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },  
+		},
+		params = {		
+			{
+				key = "shimmns708Gedreht",
+				name = _("Gedreht_aktiv"),
+				values = { "No", "Yes", },
+				tooltip = _("option_sahimms_900_1_desc"),
+				defaultIndex = 1,
+			},
+			{
+				key = "soundCheck",
+				name = _("sound_check"),
+				uiType = "CHECKBOX",
+				values = { "No", "Yes", },				
+				defaultIndex = 1,	
+			},
+		},
+		tags = { "Europe", "Vehicle", "Freight", "Wagon" },
+		dependencies = {},
+		}, 
+		
+		runFn = function (settings, modParams)
+		local params = modParams[getCurrentModId()]
+		
+			local hidden = {	
+				["axbenet_gedreht.mdl"] = true,
+				["db_gedreht.mdl"] = true,
+				["db2_gedreht.mdl"] = true,
+				["db3_gedreht.mdl"] = true,
+				["db4_gedreht.mdl"] = true,
+				["dbCargo_gedreht.mdl"] = true,
+				["dbRailship_gedreht.mdl"] = true,
+				["dbRailshipAlt_gedreht.mdl"] = true,
+				["delacher_gedreht.mdl"] = true,
+				["dr_gedreht.mdl"] = true,
+				["fsAlt_gedreht.mdl"] = true,
+				["fsModern_gedreht.mdl"] = true,
+				["fsNeu_gedreht.mdl"] = true,
+				["nacco_gedreht.mdl"] = true,
+				["ns_gedreht.mdl"] = true,
+				["obb_gedreht.mdl"] = true,
+				["railCargoAustria_gedreht.mdl"] = true,
+				["railCargoAustriaEx_gedreht.mdl"] = true,
+				["sbb_gedreht.mdl"] = true,
+				["sbbAlt_gedreht.mdl"] = true,
+				["sj_gedreht.mdl"] = true,
+				["twa_gedreht.mdl"] = true,
+				["twaAlt_gedreht.mdl"] = true,
+				["gers_gedreht.mdl"] = true,
+				["tiphook_gedreht.mdl"] = true,
+				["tiphook2_gedreht.mdl"] = true,
+				["vtg_gedreht.mdl"] = true,
+				["vtg02_gedreht.mdl"] = true,
+			}
+		
+		local modelFilter = function(fileName, data)				
+				local modelName = fileName:match('/shimmns708_([^/]*.mdl)')
+				or fileName:match('/shfimmns_([^/]*.mdl)')							
+				return (modelName==nil or hidden[modelName]~=true)				
+			end  
+		
+		if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]					
+			if params["shimmns708Gedreht"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)		
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)	
+		end	
+		
+
+		local function metadataHandler(fileName, data)
+			if params.soundCheck == 0 then
+				if fileName:match('/vehicle/waggon/shimmns708/shimmns708_([^/]*.mdl)') 
+				or fileName:match('/vehicle/waggon/sfhimmns/shfimmns_([^/]*.mdl)') 
+				or fileName:match('/vehicle/waggon/sfhimmns/gedreht/shfimmns_([^/]*.mdl)') 
+				or fileName:match('/vehicle/waggon/shimmns708/gedreht/shimmns708_([^/]*.mdl)') 
+				then
+					data.metadata.railVehicle.soundSet.name = "waggon_freight_old"
+				end
+			end
+			return data
+		end
+
+		addModifier( "loadModel", metadataHandler )
+	end,
+}
+end
---
+++ strings.lua
@@ -0,0 +1,39 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Shimmns/Sfhimmns (1978)",
+		["mod_desc"] = "Short four-axle bogie car with three hoods that can be pushed over each other, specially developed for the transport of heavy, weather-sensitive sheet metal rolls and standardized by the UIC, year of use from 1978. A similar, narrower variant was developed by LHB in 1979 for ferryboat transport and designated Sfhimmns.",
+		["asset_desc"] = "VW Containers",
+		["Shimmns_desc"] = "In 1971-1973, a short four-axle bogie car with three hoods that can be pushed over each other is developed especially for the transport of heavy, weather-sensitive sheet metal rolls and standardized by the UIC. In 1976, LHB builds a prototype that is designated Shis 708 by DB. The wagon has five troughs permanently integrated into the underframe in three different sizes to accommodate coils up to a maximum individual weight of 45 tons. The aluminum hoods can be telescoped into each other, freeing up to 2/3 of the loading area. Between 1978 and 1996, DB alone procured a total of 2,680 cars in several construction lots, and further vehicles were procured for SBB, FS and ÖBB throughout Europe.",
+		["Sfhimmns_desc"] = "For the transport of steel coils to Great Britain, Linke-Hofmann-Busch developed a special version of the shimmn as a ferryboat variant (Sfhimmns) in 1979. The wagon is 2m longer than the Shimmns 708 and also lower due to the British loading gauge. The car was employed by both DB and BR and initially ran at VTG Hamburg. Later Tiphook took over some of the former VTG wagons, then in the mid-2000s also GE Rail Services.",
+        ["gedreht"] = "(rotated) ",
+		["Gedreht_aktiv"] = "Activate rotated versions?",
+		["sound_check"] = "Activate custom sound?",
+		["height"] = "Height",
+		["offset x-axis"] = "Offset x-axis",
+		["ground"] = "Ground",
+		["rail"] = "Rail",
+		["vehicle"] = "Vehicle",
+		
+	},
+	de = {
+
+		["mod_name"] = "Shimmns/Sfhimmns (1978)",
+		["mod_desc"] = "Kurzer vierachsiger Drehgestellwagen mit drei übereinanderschiebbaren Hauben speziell für den Transport schwerer, witterungsempfindlicher Blechrollen entwickelt und von der UIC standardisiert, Einsatzjahr ab 1978. Für den Fährbootverkehr wurde 1979 von LHB eine ähnliche, schmalere Variante entwickelt und als Sfhimmns bezeichnet.",
+		["asset_desc"] = "VW Behälter",
+        ["Laabs (Volkswagen)"] = "Laabs (Volkswagen)",
+        ["Shimmns_desc"] = "1971-1973 wird ein kurzer vierachsiger Drehgestellwagen mit drei übereinanderschiebbaren Hauben speziell für den Transport schwerer, witterungsempfindlicher Blechrollen entwickelt und von der UIC standardisiert. Im Jahre 1976 baut LHB einen Prototyp, der bei der DB als Shis 708 bezeichnet wird. Der Wagen hat fünf, fest in das Untergestell integrierte Mulden in drei verschiedenen Größen für die Aufnahme von Coils bis zu einem Einzelgewicht von maximal 45 Tonnen. Die Aluminiumhauben lassen sich teleskopartig ineinander schieben und geben bis zu 2/3 der Ladefläche frei. Zwischen 1978 und 1996 beschafft alleine die DB in mehreren Baulosen insgesamt 2.680 Wagen, europaweit wurden für SBB, FS und ÖBB weitere Fahrzeuge besorgt.",
+		["Sfhimmns_desc"] = "Für den Transport von Stahl Coils nach Großbritannien entwickelte Linke-Hofmann-Busch 1979 eine spezielle Ausführung des Shimmns als Fährbootvariante (Sfhimmns). Der Wagen ist um 2m länger als der Shimmns 708 und ebenso aufgrund des des britischen Lichtraumprofils um einiges niedriger. Der Wagen war sowohl bei der DB als auch der BR eingestellt und lieft anfangs bei VTG Hamburg. Später übernahm Tiphook einige der ehemaligen VTG Wagen, Mitte der 2000 dann auch GE Rail Services.",
+        ["gedreht"] = "(gedreht) ",
+		["Gedreht_aktiv"] = "Sollen gedrehte Versionen aktiviert sein?",
+		["sound_check"] = "Eigenes Soundset aktivieren?",
+		["height"] = "Höhe",
+		["offset x-axis"] = "Verschub X-Achse",
+		["ground"] = "Boden",
+		["rail"] = "Gleis",
+		["vehicle"] = "Fahrzeug",
+
+	},
+}
+end
